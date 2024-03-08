9756229c4b9014a2a92f6daa9c995734b5dd1d5d
DeividZamora <zamoradeivid4@gmail.com>
2024-03-08 07:46:48 -0500

    ma

--- src/TELEVISORSITO/Aplicaçao.java
+++ src/TELEVISORSITO/Aplicaçao.java
@@ -1,9 +1,16 @@
 package TELEVISORSITO;
-
+import java.util.Scanner;
 public class Aplicaçao {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	
+	{ 
+		Scanner leer = new Scanner(System.in);
+		int selec;
+		System.out.println("¿Que quieres hacer?");
+		selec = leer.nextInt();
+        	
+
 
 	}
 
